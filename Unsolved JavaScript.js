92bb2f061f1f76dcbf73f2ffa8102323681ad1bf
Adam Buckley <atbuck1993@icloud.com>
2022-07-28 20:17:18 -0400

    adding new files

---
+++ Unsolved JavaScript.js
@@ -0,0 +1,13 @@
+Unsolved JavaScript
+
+function contactUs() {
+    if document.getElementById("form").submit()
+    alert("Hello, " + contactUs);
+}
+function helloBeautiful {
+let person = prompt("What's your name?")
+if (person != null) {
+document.getElementById("hello").innerHTML =
+"Hello, " + person "! Welcome to Adam's Encyclopedia of Dad Jokes, Inspirational Quotes and Feel-Good Stories!";
+}
+}
