01e2cfc59646823409e589881d5ca2b37f46cfef
Adam Buckley <atbuck1993@icloud.com>
2022-07-30 21:51:23 -0400

    Deleted and modified files for website

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function myFunction() {
     }
 }
 
+function contactUs() {
+    const fName = document.getElementById("fname").value
+    const lName = document.getElementById("lname").value
+    const subjectText = document.getElementById("subject").value
+
+    if (subjectText.length < 10) {
+        alert("Subject must be 10 characters or longer!")
+    } else {
+
+        alert("Hello, " + fName + " " + lName);
+    }
+}
+
+function helloBeautiful() {
+    let person = prompt("What's your name?")
+    if (person != null) {
+        document.getElementById("hello").innerHTML =
+            "Hello, " + person + "! Welcome to Adam's Encyclopedia of Dad Jokes, Inspirational Quotes and Feel-Good Stories!";
+    }
+}
